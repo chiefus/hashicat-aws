80c91ccb6e4556421dca0fa02f070846c86160aa
root <asshelkou@gmail.com>
2021-05-17 06:22:00 +0000

    Restrict SSH

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ resource "aws_security_group" "hashicat" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["213.184.248.184/0"]
   }
 
   ingress {
